a5095c310c94c8506cc05e4419ee48d54a119ffb
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-07-25 23:12:13 -0700

    created button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+function Button({ value, onButtonClick }) {
+    return(
+        <>
+            <button onClick={onButtonClick}>
+                {value}
+            </button>
+        </>
+    )
+}
+
+export default Button
