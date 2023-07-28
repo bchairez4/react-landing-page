996daf21045d1009ebae30d28c4ee3c92d38cde0
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-07-27 18:15:01 -0700

    reworked and redefined content component

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,9 +1,22 @@
-function Content({ content }) {
+import Post from "./Post";
+
+function Content({ posts }) {
+    const feed = posts.map((post) => {
+        return (
+            <Post
+                profile_image_src={post.profile_image_src} 
+                handle={post.handle} 
+                message={post.message} 
+                uploading_image={post.uploading_image} 
+                image_src={post.image_src} />
+        );
+    });
+
     return (
-        <p>
-            {content}
-        </p>
-    )
+        <div>
+            {feed}
+        </div>       
+    );
 }
 
-export default Content
+export default Content;
