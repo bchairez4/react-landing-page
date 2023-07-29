8220ea146c7859bf0e660ea140727621d0ba0df6
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-07-29 00:14:40 -0700

    created sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,21 @@
+import Button from "./Button";
+import Link from "./Link";
+import Profile from "./Profile";
+
+function Sidebar({ links }) {
+    const navbarLinks = links.map((link) => {
+        return (
+            <Link link_src={link.src} link_name={link.name}/>
+        );
+    });
+
+    return (
+        <div>
+            {navbarLinks}
+            <Button value={"Tweet"} />
+            <Profile image_src={"./src/assets/images/default_profilepic.png"} name={"Default"} handle={"Default"} />
+        </div>
+    );
+}
+
+export default Sidebar;
