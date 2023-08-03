a4c1daa77e207602490894af3a3398794355db4a
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-08-02 22:17:24 -0700

    added style to feed posts

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,12 +4,14 @@ import Tweet from "./Tweet";
 function Content({ posts }) {
     const feed = posts.map((post) => {
         return (
-            <Post
-                profile_image_src={post.profile_image_src} 
-                handle={post.handle} 
-                message={post.message} 
-                uploading_image={post.uploading_image} 
-                image_src={post.image_src} />
+            <div className="content-post">
+                <Post
+                    profile_image_src={post.profile_image_src} 
+                    handle={post.handle} 
+                    message={post.message} 
+                    uploading_image={post.uploading_image} 
+                    image_src={post.image_src} />
+            </div>
         );
     });
 
