e8883cf9146d7ec970a82bd39e4ed06c890d73be
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-08-02 22:19:09 -0700

    added style to sidebar links

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,14 +5,18 @@ import Profile from "./Profile";
 function Sidebar({ links }) {
     const navbarLinks = links.map((link) => {
         return (
-            <Link link_src={link.src} link_name={link.name}/>
+            <div className="sidebar-links">
+                <Link link_src={link.src} link_name={link.name}/>
+            </div>
         );
     });
 
     return (
         <div className="sidebar">
             {navbarLinks}
-            <Button value={"Tweet"} />
+            <div className="tweet-button">
+                <Button value={"Tweet"} />
+            </div>
             <Profile image_src={"./src/assets/images/default_profilepic.png"} name={"Default"} handle={"Default"} />
         </div>
     );
