0ab9c87292d523bd3b50f9c1aad14f7ff6b8b6d3
Brian Chairez <129829031+bchairez4@users.noreply.github.com>
2023-08-16 20:23:41 -0700

    updated component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,9 +4,7 @@ import Picture from "./Picture";
 function Profile({ image_src, name, handle }) {
     return (
         <div className="sidebar-profile">
-            <div className="profile-picture">
-                <Picture image_src={image_src} />
-            </div>
+            <Picture image_src={image_src} />
             <Name name={name} />
             <Name name={"@" + handle} />
         </div>
